cae240092781c9f86e64f5683dd472fe451d2cd5
Nick Hellemans <NickHellemans93@gmail.com>
2023-09-25 11:33:44 +0200

    Update resources.ts

--- config/resources.ts
+++ config/resources.ts
@@ -1,2 +1,4 @@
 export const resources =
-  'https://lucasfcosta.com/2023/02/07/backlogs-are-useless.html'
+  ['https://lucasfcosta.com/2023/02/07/backlogs-are-useless.html',
+   'https://vimeo.com/297977944'
+   ]
